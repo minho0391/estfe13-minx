9791b4e1ea1fabca3b8cbfbe6d86db3a6dc5fb1c
minho0391 <minh05@naver.com>
2026-07-13 10:35:48 +0900

    Implement authentication features, including user login/signup, profile navigation, and comments functionality

--- src/routes/Auth.jsx
+++ src/routes/Auth.jsx
@@ -1,5 +1,12 @@
-import { useState } from "react";
-import { Box, Typography, TextField, Button } from "@mui/material";
+import { useState, useRef } from "react";
+import { Box, Typography, TextField, Button, Divider, Snackbar } from "@mui/material";
+import { authService } from "../firebase";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
 
 function Auth() {
   const [newAccount, setNewAccount] = useState(true);
@@ -7,7 +14,13 @@ function Auth() {
     email: "",
     password: "",
   });
-  console.log(form);
+  const [error, setError] = useState("");
+  const [errorOpen, setErrorOpen] = useState(false);
+
+  const emailRef = useRef(null);
+
+  const auth = authService;
+  const provider = new GoogleAuthProvider();
 
   const handleChange = e => {
     const { name, value } = e.target;
@@ -16,12 +29,70 @@ function Auth() {
       [name]: value,
     }));
   };
+
+  const onSubmit = e => {
+    e.preventDefault();
+    if (newAccount) {
+      //회원가입
+      createUserWithEmailAndPassword(auth, form.email, form.password)
+        .then(userCredential => {
+          // Signed up
+          const user = userCredential.user;
+          // ...
+        })
+        .catch(error => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log(errorCode, errorMessage);
+          setError(
+            errorMessage.includes("email-already-in-use")
+              ? "이메일이 이미 사용중입니다."
+              : errorMessage,
+          ); //에러메시지 생성
+          setErrorOpen(true);
+          setForm({ email: "", password: "" });
+          emailRef.current.focus();
+        });
+    } else {
+      //로그인
+      signInWithEmailAndPassword(auth, form.email, form.password)
+        .then(userCredential => {
+          // Signed in
+          const user = userCredential.user;
+          // ...
+        })
+        .catch(error => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log(errorCode, errorMessage);
+          setError(errorMessage); //에러 메시지 생성
+          setErrorOpen(true);
+          setForm({ email: "", password: "" });
+          emailRef.current.focus();
+        });
+    }
+  };
+  const onGoogleSignIn = () => {
+    signInWithPopup(auth, provider)
+      .then(result => {
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        const user = result.user;
+      })
+      .catch(error => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        const email = error.customData.email;
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        console.log(errorCode, errorMessage, email, credential);
+      });
+  };
   return (
     <>
       <Typography variant="h2" component="h2">
-        Login Form
+        {newAccount ? "회원가입 폼" : "로그인 폼"}
       </Typography>
-      <Box component="form" sx={{ mt: 2 }}>
+      <Box component="form" sx={{ mt: 2 }} onSubmit={onSubmit}>
         <TextField
           fullWidth
           label="Email address"
@@ -29,18 +100,48 @@ function Auth() {
           name="email"
           variant="outlined"
           onChange={handleChange}
+          inputRef={emailRef}
+          value={form.email}
         />
         <TextField
           sx={{ mt: 2 }}
           fullWidth
           label="Password"
-          type="Password"
-          name="Password"
+          type="password"
+          name="password"
           variant="outlined"
           onChange={handleChange}
+          value={form.password}
         />
         <Button sx={{ mt: 2 }} type="submit" variant="contained">
-          Login
+          {newAccount ? "회원가입" : "로그인"}
+        </Button>
+
+        <Snackbar
+          open={errorOpen}
+          autoHideDuration={3000}
+          message={error}
+          onClose={() => {
+            setErrorOpen(false);
+          }}
+        />
+
+        <Divider sx={{ my: 3 }} />
+
+        <Button sx={{ mt: 2 }} type="button" variant="contained" onClick={onGoogleSignIn}>
+          {newAccount ? "구글로 회원가입" : "구글로 로그인"}
+        </Button>
+
+        <Divider sx={{ my: 3 }} />
+        <Button
+          sx={{ mt: 2 }}
+          type="button"
+          variant="contained"
+          onClick={() => {
+            setNewAccount(prev => !prev);
+          }}
+        >
+          {newAccount ? "로그인으로 전환" : "회원가입으로 전환"}
         </Button>
       </Box>
     </>
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,7 +1,83 @@
+import {
+  Box,
+  Typography,
+  TextField,
+  Button,
+  Divider,
+  useScrollTrigger,
+  ListItem,
+  List,
+  ListItemText,
+} from "@mui/material";
+import { collection, addDoc, serverTimestamp, query, where, getDocs } from "firebase/firestore";
+import { db } from "../firebase";
+import { useEffect, useState } from "react";
+
 function Home() {
+  const [comment, setComment] = useState("");
+  const [comments, setComments] = useState([]);
+  /*
+  useEffect로 데이터를 조회 결과를 변수명 comments할당
+  */
+  const getComments = async () => {
+    const q = query(collection(db, "comments"));
+    const querySnapshot = await getDocs(q);
+    console.log(querySnapshot);
+    const commentsArray = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+    setComments(commentsArray);
+  };
+  useEffect(() => {
+    getComments();
+  }, []);
+  const handleChange = e => {
+    setComment(e.target.value);
+  };
+  const onSubmit = async e => {
+    e.preventDefault();
+    try {
+      const docRef = await addDoc(collection(db, "comments"), {
+        // comment: comment,
+        comment,
+        date: serverTimestamp(),
+      });
+      setComment("");
+    } catch (e) {
+      console.error("글 추가시 에러가 발생했습니다.", e);
+    }
+  };
   return (
     <>
-      <h2>Home</h2>
+      <Typography variant="h2" component="h2">
+        Home{" "}
+      </Typography>
+
+      <Box component="form" sx={{ mt: 2 }} onSubmit={onSubmit}>
+        <TextField
+          fullWidth
+          label="Comment"
+          placeholder="글을 입력해주세요."
+          type="text"
+          name="comment"
+          variant="outlined"
+          multiline
+          rows={5}
+          value={comment}
+          onChange={handleChange}
+        />
+        <Button sx={{ mt: 2 }} type="submit" variant="contained">
+          글쓰기
+        </Button>
+      </Box>
+      <Divider sx={{ my: 3 }} />
+      <List sx={{ width: "100%", bgcolor: "background.paper" }}>
+        <ListItem alignItems="flex-start"></ListItem>
+        {/* commets 배열의 값을 ListItem으로 출력 */}
+        {comments.map(item => (
+          <ListItem key={item.id} alignItems="flex-start" Divider>
+            <ListItemText primary={item.comment} secondary={item.date.toDate().toLocaleString()} />
+          </ListItem>
+        ))}
+      </List>
     </>
   );
 }
